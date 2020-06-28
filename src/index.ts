8686eee8fa7647dc2622dc6deb7ba791e43d0fa7
EOussama <oussama_gtacj@live.fr>
2020-06-28 22:28:55 +0100

    Proper setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hello, World!');
+import * as config from "./config.json";
+
+/**
+ * Anusic scrapper class
+ */
+export default class AnusicScrapper {
+
+  //#region Properties
+  //#endregion
+
+  //#region Lifecycle
+
+  constructor() { }
+
+  //#endregion
+
+  //#region Methods
+  //#endregion
+}
+
+new AnusicScrapper();
