2f49015954d5fb425a4c763f6ea1d3b467b9b5bb
EOussama <oussama_gtacj@live.fr>
2020-11-20 22:37:47 +0100

    Added alt and ID extractions

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -37,22 +38,68 @@ func animeList(w http.ResponseWriter, r *http.Request) {
 	collector := colly.NewCollector()
 
 	// Initializing the anime list
-	animeCol := []Anime{}
+	animeTitles := []Anime{}
+
+	var currAnime string = ""
 
 	// Scraping the catalog
 	collector.OnHTML(".wiki", func(e *colly.HTMLElement) {
-		e.ForEach("#wiki_0-9 ~ p a", func(_ int, element *colly.HTMLElement) {
+
+		// Anime list
+		e.ForEach("#wiki_0-9 ~ p a", func(index int, element *colly.HTMLElement) {
+
+			// Extracting anime title
 			extract := element.Text
 			idx := strings.LastIndex(extract, " (")
 			year, _ := strconv.ParseInt(extract[idx+2:len(extract)-1], 10, 16)
 
 			anime := Anime{
-				Name: extract[:idx],
-				Year: uint16(year),
+				Name:     extract[:idx],
+				Year:     uint16(year),
+				linkInfo: element.Attr("href"),
 			}
 
-			animeCol = append(animeCol, anime)
+			// Appending extracted anime title
+			animeTitles = append(animeTitles, anime)
+
+			currAnime = anime.linkInfo[strings.Index(anime.linkInfo, "#")+1:]
+			collector.Visit("https://www.reddit.com" + anime.linkInfo)
 		})
+
+		// Anime info
+		if len(currAnime) > 0 {
+
+			// Getting all h3 elements
+			e.ForEach("h3", func(_ int, element *colly.HTMLElement) {
+
+				// Checking if the ID matches the current anime title
+				if element.Attr("id") == currAnime {
+
+					// Getting the target anime
+					anime := &animeTitles[len(animeTitles)-1]
+
+					// Extracting the ID
+					mal := element.ChildAttr("a", "href")
+					idx := strings.LastIndex(mal, "/anime/")
+					extr := mal[idx+len("/anime/") : len(mal)-1]
+					id, err := strconv.ParseInt(extr, 10, 32)
+
+					if err == nil {
+						anime.ID = uint16(id)
+					}
+
+					// Extracting the alt name
+					if element.DOM.Next().Is("p") {
+						anime.AltName = element.DOM.Next().Text()
+					}
+
+					fmt.Printf("%+v\n", animeTitles[len(animeTitles)-1])
+				}
+			})
+
+			// Clearing the current anime
+			currAnime = ""
+		}
 	})
 
 	collector.OnRequest(func(req *colly.Request) {
@@ -70,5 +117,5 @@ func animeList(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	// Encoding the return value
-	json.NewEncoder(w).Encode(animeCol)
+	json.NewEncoder(w).Encode(animeTitles)
 }
--- src/model.anime.go
+++ src/model.anime.go
@@ -2,9 +2,10 @@ package main
 
 // Anime type
 type Anime struct {
-	ID      string  `json:"id"`
-	Name    string  `json:"name"`
-	AltName string  `json:"altName"`
-	Year    uint16  `json:"year"`
-	Themes  []Theme `json:"themes"`
+	ID       uint16  `json:"id"`
+	Name     string  `json:"name"`
+	AltName  string  `json:"altName"`
+	Year     uint16  `json:"year"`
+	Themes   []Theme `json:"themes"`
+	linkInfo string
 }
