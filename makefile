e8af743ee69db39e2a5a4c702c604b784fd72036
EOussama <oussama_gtacj@live.fr>
2020-11-20 15:20:33 +0100

    Added main route and data structs

---
+++ makefile
@@ -0,0 +1,5 @@
+run: 
+	go run ./src/main.go
+
+build:
+	go build ./src/main.go
--- src/main.go
+++ src/main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Media format
+const (
+	MediaFormatWebm = iota
+)
+
+// Theme song types
+const (
+	ThemeTypeOP = iota
+	ThemeTypeED = iota
+)
+
+// Theme song type
+type Theme struct {
+	name         string
+	link         string
+	episodes     []string
+	themeType    int8
+	format       int8
+	resolution   string
+	hasSpoilers  bool
+	isNSFW       bool
+	isCreditless bool
+	hasLyrics    bool
+	isTransition bool
+	isOver       bool
+}
+
+// Anime type
+type Anime struct {
+	id      string
+	name    string
+	altName string
+	themes  []Theme
+}
 
 func main() {
-	fmt.Println("Anusic API!s")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", index)
+
+	http.ListenAndServe(":8000", r)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+
 }
