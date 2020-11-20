53133c3a859bea3c6113ddb9a8156129b774f5ae
EOussama <oussama_gtacj@live.fr>
2020-11-20 18:31:04 +0100

    Added catalog scraper

--- src/main.go
+++ src/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/gocolly/colly"
 	"github.com/gorilla/mux"
 )
 
@@ -12,26 +13,47 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", index).Methods("GET")
+	r.HandleFunc("/anime", animeList).Methods("GET")
 
 	http.ListenAndServe(":8000", r)
 }
 
+// Entry
 func index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode("Anusic API")
+}
 
-	themes := []Theme{}
-	themes = append(themes, Theme{
-		Name: "dddd",
+// Get anime list
+func animeList(w http.ResponseWriter, r *http.Request) {
+
+	// Initializing the scraper
+	c := colly.NewCollector()
+
+	// Catalog list (used to group target entries)
+	catalog := []string{}
+
+	// Initializing the anime list
+	// animeCol := []string{}
+
+	// Scraping the catalog
+	c.OnHTML(".toc", func(e *colly.HTMLElement) {
+		e.ForEach("li", func(_ int, element *colly.HTMLElement) {
+			catalog = append(catalog, element.Text)
+		})
 	})
 
-	anime := Anime{
-		Name:    ".hack//Liminality",
-		AltName: "",
-		ID:      "",
-		Year:    2002,
-		Themes:  themes,
-	}
+	fmt.Println(catalog)
 
-	fmt.Println(anime)
-	json.NewEncoder(w).Encode(anime)
+	// Visiting the target page and invoking the scraper
+	c.Visit("https://www.reddit.com/r/AnimeThemes/wiki/anime_index")
+
+	// Setting up JSON headers
+	w.Header().Set("Content-Type", "application/json")
+
+	// Encoding the return value
+	json.NewEncoder(w).Encode("Anime list")
 }
+
+// https://www.reddit.com/r/AnimeThemes/wiki
+// "github.com/gocolly/colly"
