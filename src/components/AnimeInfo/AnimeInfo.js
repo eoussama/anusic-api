9ff69a6fb03a85e25887a0ce290c4de989058876
EOussama <oussama_gtacj@live.fr>
2020-12-13 12:31:58 +0100

    Added testing links

--- src/components/AnimeInfo/AnimeInfo.js
+++ src/components/AnimeInfo/AnimeInfo.js
@@ -45,12 +45,20 @@ export default class AnimeInfo extends Component {
                         >
                           <a target="_blank"
                             href={source.link}>Video</a>
-                          <audio controls>
+                          <a target="_blank"
+                            href="https://www.google.com/">Google 1</a>
+                          <a target="_blank"
+                            href={'https://www.google.com/'}>Google 2</a>
+                          <a target="_blank"
+                            href="https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png">Cat 1</a>
+                          <a target="_blank"
+                            href={'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png'}>Cat 2</a>
+                          {/* <audio controls>
                             <source
                               src={source.link}
                               type="audio/ogg"
                             ></source>
-                          </audio>
+                          </audio> */}
                         </li>
                       ))
                     }
